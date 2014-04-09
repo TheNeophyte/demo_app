58437d2eb4ae51e227bcdf5292eccd95fc06053a
Niwt Live <phlip.wilson@gmail.com>
2014-04-09 10:55:54 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = '10f71fb4ca8ccb552e46ae57478109dc0f103c7d89ba37d3b08404ec3d507157dc72e5cef58b0a81b1f077f37fb359c46c02dcd826389684923a23905333aadf'
